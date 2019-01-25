324cc98dbf12cd80893229e47ca0618b7fb73958
Zorigt Batbold <38428606+bzorigt@users.noreply.github.com>
2019-01-25 14:28:16 +0800

    Update lazyinstaller.sh
    Added gnome tweak tool.

--- lazyinstaller.sh
+++ lazyinstaller.sh
@@ -34,6 +34,9 @@ sudo apt update
 BLUE "Installing git..."
 sudo apt install -y git
 
+BLUE "Installing gnome tweak tool..."
+sudo apt install -y gnome-tweak-tool
+
 BLUE "Installing Sublime Text..." # according to https://www.sublimetext.com/docs/3/linux_repositories.h$
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install -y apt-transport-https
